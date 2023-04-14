e70c20cfd4e3b7ef79969ec34569969fdb9a6300
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-04-14 23:30:44 +0300

    feat: added header

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,13 @@
-import Header from "../modules/Header";
+import {HeaderMiddle} from "../modules/Header";
 
 export const Home = () => {
+    const links = [
+        { link: "/", label: "Home" },
+        { link: "/about", label: "About" },
+        { link: "/contact", label: "Contact" },
+    ];
+
     return (
-        <Header/>
+        <HeaderMiddle links={links} />
     );
 }
