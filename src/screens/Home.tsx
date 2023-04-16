2c08e91804d82bee3f2300818cafbc0165f22e63
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-04-16 21:44:09 +0300

    fix: remove unused imports

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,3 @@
-import {HeaderModule} from "../modules/Header";
-import {FaGithub, FaLinkedinIn, FaTelegramPlane} from 'react-icons/fa';
-
 export const Home = () => {
     return (
         <h1>fdsfsfs</h1>
