91be33d87b4459ebf879b0c3da3fd0fe44718fd9
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-04-16 17:58:31 +0300

    feat: refactor header

--- src/modules/Header/components/Links/styles/index.ts
+++ src/modules/Header/components/Links/styles/index.ts
@@ -1,48 +1,6 @@
 import {createStyles, rem} from "@mantine/core";
 
-export const HEADER_HEIGHT = rem(60);
-
 export const useStyles = createStyles((theme) => ({
-    root: {
-        position: 'relative',
-        zIndex: 1,
-    },
-
-    dropdown: {
-        position: 'absolute',
-        top: HEADER_HEIGHT,
-        left: 0,
-        right: 0,
-        zIndex: 0,
-        borderTopRightRadius: 0,
-        borderTopLeftRadius: 0,
-        borderTopWidth: 0,
-        overflow: 'hidden',
-
-        [theme.fn.largerThan('sm')]: {
-            display: 'none',
-        },
-    },
-
-    header: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: '100%',
-    },
-
-    links: {
-        [theme.fn.smallerThan('sm')]: {
-            display: 'none',
-        },
-    },
-
-    burger: {
-        [theme.fn.largerThan('sm')]: {
-            display: 'none',
-        },
-    },
-
     link: {
         display: 'block',
         lineHeight: 1,
--- src/modules/Header/styles/index.ts
+++ src/modules/Header/styles/index.ts
@@ -43,30 +43,4 @@ export const useStyles = createStyles((theme) => ({
         },
     },
 
-    link: {
-        display: 'block',
-        lineHeight: 1,
-        padding: `${rem(8)} ${rem(12)}`,
-        borderRadius: theme.radius.sm,
-        textDecoration: 'none',
-        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-        fontSize: theme.fontSizes.sm,
-        fontWeight: 500,
-
-        '&:hover': {
-            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        },
-
-        [theme.fn.smallerThan('sm')]: {
-            borderRadius: 0,
-            padding: theme.spacing.md,
-        },
-    },
-
-    linkActive: {
-        '&, &:hover': {
-            backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
-            color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
-        },
-    },
 }));
