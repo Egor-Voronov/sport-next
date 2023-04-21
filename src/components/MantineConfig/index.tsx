93723f0843da5050ca96bbdae2276af1c6f06269
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-04-21 22:39:15 +0300

    feat: created a save of the current color scheme into local storage & preferred theme identification

--- src/components/MantineConfig/index.tsx
+++ src/components/MantineConfig/index.tsx
@@ -1,11 +1,19 @@
-import {useState} from 'react';
 import {ColorScheme, ColorSchemeProvider, MantineProvider} from '@mantine/core';
 import {IMantineConfigProps} from "./types"
+import {useColorScheme, useLocalStorage} from '@mantine/hooks';
+
 
 export const MantineConfig = ({children}: IMantineConfigProps) => {
-    const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+    const preferredColorScheme = useColorScheme();
+    const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+        key: 'color-scheme',
+        defaultValue: preferredColorScheme,
+        getInitialValueInEffect: true,
+    });
+
     const toggleColorScheme = (value?: ColorScheme) =>
         setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
     return (
         <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
             <MantineProvider
