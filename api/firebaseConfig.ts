c8742f85c1127001c494046b81a702a674c301df
corvus <vip.egor.voronov.2016@mail.ru>
2023-06-05 01:36:37 +0300

    init: setup firebase config

---
+++ api/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVtqCCZcQ3wYqMpTokZSxQuW5Pn9QVG90",
+  authDomain: "sport-next.firebaseapp.com",
+  projectId: "sport-next",
+  storageBucket: "sport-next.appspot.com",
+  messagingSenderId: "983586181533",
+  appId: "1:983586181533:web:d9f8e67155324301c89f74",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
